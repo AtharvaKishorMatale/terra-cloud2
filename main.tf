3c78a0056712ea2e75308376a25dbe73a97d7737
Atharva Kishor Matale <160848806+AtharvaKishorMatale@users.noreply.github.com>
2025-05-17 12:48:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "myec2" {
   ami                    = var.ami_id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.mysg.id]
-  key_name               = "demo"
+  key_name               = "peer"
   tags = {
     Name = "myinstance"
   }
